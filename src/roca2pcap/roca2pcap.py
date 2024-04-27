9f834975cb44e1ae5097ae503b02ecde94d0725d
David Groves <git@fibrecat.org>
2024-04-27 16:34:28 +0100

    Initial Coding.

--- src/roca2pcap/roca2pcap.py
+++ src/roca2pcap/roca2pcap.py
@@ -1,8 +1,11 @@
 import base64
 import dataclasses
-import typing
+import macaddress
+import pathlib
 import sys
-import pprint
+import typing
+
+import scapy.all as scapy  # type: ignore
 
 
 @dataclasses.dataclass
@@ -15,45 +18,96 @@ class Packet:
     def __repr__(self):
         return f"Packet(timestamp={self.timestamp}, count={self.count}, ethertype={self.ethertype}, packet_content={self.packet_content.hex()})"
 
+    @property
+    def dstmac_bytes(self) -> bytes:
+        return self.packet_content[0:6]
+
+    @property
+    def srcmac_bytes(self) -> bytes:
+        return self.packet_content[6:11]
+
+    @property
+    def srcmac(self) -> str:
+        return str(macaddress.MAC(self.srcmac_bytes)).replace("-", ":")
+        
+    @property
+    def dstmac(self) -> str:
+        return str(macaddress.MAC(self.dstmac_bytes)).replace("-", ":")
+    
+
+    @property
+    def l3payload(self) -> bytes:
+        return self.packet_content[14:]
+
+
     @classmethod
     def from_string(cls, line: str) -> typing.Self:
         # Parse the line into components.
         try:
             timestamp, count, reserved, ethertype, contents = line.split(",")
-        except ValueError:
-            sys.stderr.write(f"Could not parse line {line}.")
+        except ValueError as e:
+            sys.stderr.write(f"Could not parse line {line}.\n")
             raise e
 
         # Convert the components into correct types.
         try:
-            cls.timestamp = int(timestamp)
+            ret_timestamp = int(timestamp)
         except Exception as e:
-            sys.stderr.write(f"Could not parse timestamp (first field) in {line}.")
+            sys.stderr.write(f"Could not parse timestamp (first field) in {line}.\n")
             raise e
+        if ret_timestamp < 0:
+            sys.stderr.write(f"Timestamp is negative (first field) in {line}.\n")
+            raise ValueError("Timestamp is negative.")
 
         try:
-            cls.count = int(count)
+            ret_count = int(count)
         except Exception as e:
-            sys.stderr.write(f"Could not parse count (second field) in {line}.")
+            sys.stderr.write(f"Could not parse count (second field) in {line}.\n")
             raise e
+        if ret_count < 0:
+            sys.stderr.write(f"Count is negative (second field) in {line}.\n")
+            raise ValueError("Count is negative.")
 
         if reserved != "0":
-            sys.stderr.write("Purpose of third field is unknown, only 0 is currently accepted.")
-            sys.stderr.write("Please contact the developer of this application to report")
-            sys.stderr.write("this new discovery !")
-            sys.stderr.write(f"Your line: {line}")
+            sys.stderr.write("Purpose of third field is unknown, only 0 is currently accepted.\n")
+            sys.stderr.write("Please contact the developer of this application to report\n")
+            sys.stderr.write("this new discovery !\n")
+            sys.stderr.write(f"Your line: {line}\n")
             raise ValueError("Non 0 reserved field found.")
 
         try:
-            cls.ethertype = int(ethertype)
+            ret_ethertype = int(ethertype)
         except Exception as e:
-            sys.stderr.write(f"Could not parse ethertype (fourth field) in {line}")
+            sys.stderr.write(f"Could not parse ethertype (fourth field) in {line}\n")
             raise e
 
         try:
-            cls.packet_content = base64.b64decode(contents)
+            ret_packet_content = base64.b64decode(contents, validate=True)
         except Exception as e:
-            sys.stderr.write(f"Could not parse base64 packet contents (fifth field) in {line}")
+            sys.stderr.write(f"Could not parse base64 packet contents (fifth field) in {line}\n")
             raise e
 
-        return cls
+        return cls(ret_timestamp, ret_count, ret_ethertype, ret_packet_content)
+
+    def to_scapy(self) -> scapy.Ether:
+        s = scapy.Ether(self.packet_content)
+        s.time = self.timestamp / 1000
+        s.microsecond = self.count
+        return s
+
+def read_file(filename: pathlib.Path) -> typing.Generator[Packet, None, None]:
+    """Read a file of roca lines and generate Packet objects from it."""
+    with open(filename, "r") as f:
+        for line in f:
+            yield Packet.from_string(line)
+
+
+def main():
+    output_filename = "output.pcap"
+    with scapy.PcapWriter(filename=output_filename) as writer:
+        for packet in read_file("input.roca"):
+            writer.write(packet.to_scapy())
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_main.py
@@ -0,0 +1,59 @@
+import pytest
+import roca2pcap.roca2pcap as r2p
+import scapy.all as scapy
+
+@pytest.fixture
+def ping_test_packet():
+    return "1714050818223,0,0,2048,qrvM3e7/dKy5qFyfCABFAABUAAAAAHMBh+EICAgIrBsDnQAAAbeb9AADGK8qZgAAAABZaQcAAAAAABAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4vMDEyMzQ1Njc="
+
+
+def test_ping_packet_dpkt(ping_test_packet):
+    packet = r2p.Packet.from_string(ping_test_packet)
+    dpkt_packet = packet.to_scapy()
+    print("*****")
+    print(dpkt_packet)
+
+def test_ping_packet_parses(ping_test_packet):
+    packet = r2p.Packet.from_string(ping_test_packet)
+    assert packet.timestamp == 1714050818223
+    assert packet.ethertype == 2048
+    assert packet.count == 0
+    assert packet.packet_content == b'\xaa\xbb\xcc\xdd\xee\xfft\xac\xb9\xa8\\\x9f\x08\x00E\x00\x00T\x00\x00\x00\x00s\x01\x87\xe1\x08\x08\x08\x08\xac\x1b\x03\x9d\x00\x00\x01\xb7\x9b\xf4\x00\x03\x18\xaf*f\x00\x00\x00\x00Yi\x07\x00\x00\x00\x00\x00\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./01234567'
+
+def test_ping_dstmac(ping_test_packet):
+    packet = r2p.Packet.from_string(ping_test_packet)
+    assert packet.dstmac_bytes == b'\xaa\xbb\xcc\xdd\xee\xff'
+
+def test_ping_srcmac(ping_test_packet):
+    packet = r2p.Packet.from_string(ping_test_packet)
+    assert packet.srcmac_bytes == b't\xac\xb9\xa8\\'
+
+def test_ping_l3payload(ping_test_packet):
+    packet = r2p.Packet.from_string(ping_test_packet)
+    print(packet.l3payload)
+    assert packet.l3payload == b'E\x00\x00T\x00\x00\x00\x00s\x01\x87\xe1\x08\x08\x08\x08\xac\x1b\x03\x9d\x00\x00\x01\xb7\x9b\xf4\x00\x03\x18\xaf*f\x00\x00\x00\x00Yi\x07\x00\x00\x00\x00\x00\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./01234567'
+
+def test_timestamp_not_a_number():
+    with pytest.raises(ValueError):
+        r2p.Packet.from_string("ABCD,0,0,2048,CA==")
+
+def test_timestamp_is_negative():
+    with pytest.raises(ValueError):
+        r2p.Packet.from_string("-1,0,0,2048,CA==")
+
+def test_count_is_negative():
+    with pytest.raises(ValueError):
+        r2p.Packet.from_string("1714050818223,-1,0,2048,CA==")
+
+def test_packet_content_is_not_base64():
+    with pytest.raises(ValueError):
+        r2p.Packet.from_string("1714050818223,0,0,2048,$%^&*(){}")
+
+@pytest.mark.skip(reason="Test is correct, but data is not")
+def test_src_in_text_matches_packet(ping_test_packet):
+    packet = r2p.Packet.from_string(ping_test_packet)
+    print(packet.srcmac)
+    print(type(packet.srcmac))
+    
+    assert packet.srcmac.replace("-", ":") == packet.to_scapy().src
+    
